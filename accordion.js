cd8eeb204cd0796586a9875e44325c2f05ba71a8
Pietro Tallarico <pietrowebdev@gmail.com>
2025-04-12 16:42:25 -0700

    add effect that hides open FAQ rows when another is clicked on

--- accordion.js
+++ accordion.js
@@ -1,12 +1,19 @@
 const buttons = document.querySelectorAll(".question-button");
 
-buttons.forEach((e) => {
-  e.addEventListener("click", () => {
-    const faqRow = e.closest(".faq-row");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    //Close buttons
+    document.querySelectorAll(".faq-row").forEach((row) => {
+      row.querySelector(".text").classList.remove("text-show");
+      row.querySelector(".plus").classList.remove("hide");
+      row.querySelector(".minus").classList.remove("minus-show");
+    });
+
+    const faqRow = button.closest(".faq-row");
 
     const text = faqRow.querySelector(".text");
-    const plus = e.querySelector(".plus");
-    const minus = e.querySelector(".minus");
+    const plus = button.querySelector(".plus");
+    const minus = button.querySelector(".minus");
 
     text.classList.toggle("text-show");
     plus.classList.toggle("hide");
